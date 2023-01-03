20cb1347e70a545854b5acd5a213094f97fb2bd7
Shawn Roller <sroller@techstyle.com>
2023-01-02 21:02:24 -0500

    Initial Commit

---
+++ NShapeSets/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  NShapeSets
+//
+//  Created by Shawn Roller on 1/2/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NShapeSets/NShapeSetsApp.swift
@@ -0,0 +1,17 @@
+//
+//  NShapeSetsApp.swift
+//  NShapeSets
+//
+//  Created by Shawn Roller on 1/2/23.
+//
+
+import SwiftUI
+
+@main
+struct NShapeSetsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
