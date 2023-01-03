3e270e6db10a154f42f9abbf8b4c53b71ca17a21
Shawn Roller <sroller@techstyle.com>
2023-01-02 21:59:58 -0500

    Adds color assets.

---
+++ NShapeSets/Primitives/BackgroundGradient.swift
@@ -0,0 +1,20 @@
+//
+//  BackgroundGradient.swift
+//  NShapeSets
+//
+//  Created by Shawn Roller on 1/2/23.
+//
+
+import SwiftUI
+
+struct BackgroundGradient: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct BackgroundGradient_Previews: PreviewProvider {
+    static var previews: some View {
+        BackgroundGradient()
+    }
+}
