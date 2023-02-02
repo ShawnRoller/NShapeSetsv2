e665eb737a62b5a3e0ecd13f7496ed2eb2e297e3
Shawn Roller <sroller@techstyle.com>
2023-02-02 17:47:24 -0500

    Adds better animation for action sheet.

--- NShapeSets/Primitives/ActionSheet.swift
+++ NShapeSets/Primitives/ActionSheet.swift
@@ -10,6 +10,11 @@ import SwiftUI
 struct ActionSheet<Content: View>: View {
     var content: () -> Content
     @State private var offset: CGFloat = 0
+    @State private var isExpanded = true
+    
+    let maxOffset: CGFloat = 275
+    let pivotPoint: CGFloat = 150
+    let overshot: CGFloat = 10
     
     init(@ViewBuilder content: @escaping () -> Content) {
         self.content = content
@@ -24,42 +29,45 @@ struct ActionSheet<Content: View>: View {
                         Capsule()
                             .frame(width: 36, height: 10)
                             .foregroundColor(Palette.actionSheetGradientColors.last)
+                            .padding(10)
+                            .contentShape(Rectangle())
                         Spacer()
                     }
                     .frame(height: 30)
-                    .offset(y: -10)
                     .gesture(
                         DragGesture(coordinateSpace: .global)
                             .onChanged { value in
-                                print("on change value: \(value)")
-                                print("on change \(value.translation.height)")
-                                if value.translation.height > 0 {
+                                if isExpanded && value.translation.height > 0 {
                                     offset = value.translation.height
+                                } else if !isExpanded && value.translation.height < 0 {
+                                    let newOffset = maxOffset + value.translation.height
+                                    offset = max(newOffset, -overshot)
                                 }
                             }
                             .onEnded { value in
-                                print("on ended value: \(value)")
-                                if value.translation.height > 150 {
-                                    offset = 275
+                                if value.translation.height > pivotPoint {
+                                    offset = maxOffset
+                                    isExpanded = false
                                 } else {
                                     offset = 0
+                                    isExpanded = true
                                 }
                             }
                     )
                     Spacer()
                     content()
                 }
-                    .padding()
-                    .frame(minWidth: 0,
-                           maxWidth: .infinity,
-                           minHeight: 50,
-                           maxHeight: 400)
-                    .background(Palette.actionSheetGradient)
-                    .cornerRadius(50, corners: [.topLeft, .topRight])
+                .padding()
+                .frame(minWidth: 0,
+                       maxWidth: .infinity,
+                       minHeight: 50,
+                       maxHeight: 400)
+                .background(Palette.actionSheetGradient)
+                .cornerRadius(50, corners: [.topLeft, .topRight])
         }
         .ignoresSafeArea()
-        .offset(y: offset)
-        .animation(.easeInOut, value: offset)
+        .offset(y: offset + (overshot * 2))
+        .animation(.spring(response: 0.5, dampingFraction: 1, blendDuration: 0.5), value: offset)
     }
 }
 
