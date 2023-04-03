57a9b229d27cfc5ff584d0bdcec622b0d2003b39
Shawn Roller <shawn@rollersoft.io>
2023-04-03 18:05:41 -0400

    Updates CTA button title.

--- NShapeSets/Components/SetupActionSheet.swift
+++ NShapeSets/Components/SetupActionSheet.swift
@@ -10,6 +10,20 @@ import SwiftUI
 struct SetupActionSheet: View {
     @Binding var workout: Workout
     @Binding var isExpanded: Bool
+    private var buttonTitle: String {
+        var title = "Start"
+        switch workout.state {
+        case .Setup:
+            title = "State"
+        case .Active:
+            title = "Rest"
+        case .Rest:
+            title = "Skip"
+        case .Recap:
+            title = "Done"
+        }
+        return title
+    }
     
     var body: some View {
         ActionSheet(isExpanded: $isExpanded) {
@@ -18,7 +32,7 @@ struct SetupActionSheet: View {
                 SetsSelector(setsValue: $workout.rounds)
             }
             Footer {
-                CTAButton(title: "Start", action: { isExpanded.toggle() })
+                CTAButton(title: buttonTitle, action: { isExpanded.toggle() })
             }
         }
     }
--- NShapeSets/Documentation.docc/Documentation.md
+++ NShapeSets/Documentation.docc/Documentation.md
@@ -10,7 +10,7 @@ NShape Sets built with SwiftUI (and a bit of UIKit)
 
 - Make the ContentView work
     - [x] The Sliders need to update the Workout value appropriately
-    - Start button should send to the ActiveWorkout content
+    - [x] Start button should send to the ActiveWorkout content
     - Start button title should change depending on whether the workout is started, and whether it's Rest mode or Active mode (Start, Rest, Skip)
     
     
